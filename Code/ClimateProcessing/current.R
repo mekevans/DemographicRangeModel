7c4747e43216136b4647f95918f7fdba22f908d5
mekevans <margaret.ekevans@gmail.com>
2018-12-21 16:26:49 -0700

    climate processing on Evans computer

--- Code/ClimateProcessing/current.R
+++ Code/ClimateProcessing/current.R
@@ -2,57 +2,44 @@ library(raster)
 library(rgdal)
 
 # Read PRISM normals
-prism_files <- list.files(path = "D:/EvansLab/Final/Data/PRISM/Normals/", pattern = "bil$", full.names = T)
+PRISM.norm.path <-  "E:/Bayes/DemogRangeMod/ProofOfConcept/FIA-data/westernData/NewData/IWStates/PiedIPM/MEKEvans/ClimateData/PRISM/Normals/"
+prism_files <- list.files(path = PRISM.norm.path, pattern = "bil$", full.names = T)
 prism <- stack(prism_files)
 
-# Create seasonal normals
-PPT_cool <- 
-  prism$PRISM_ppt_30yr_normal_800mM2_10_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_11_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_12_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_01_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_02_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_03_bil
-VPDmin_cool <- (
-  prism$PRISM_vpdmin_30yr_normal_800mM2_10_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_11_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_12_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_01_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_02_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_03_bil) / 6
-VPDmax_cool <- (
-  prism$PRISM_vpdmax_30yr_normal_800mM2_10_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_11_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_12_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_01_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_02_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_03_bil) / 6
-VPD_cool <- (VPDmin_cool + VPDmax_cool) / 2
-
-PPT_warm <- 
-  prism$PRISM_ppt_30yr_normal_800mM2_05_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_06_bil + 
-  prism$PRISM_ppt_30yr_normal_800mM2_07_bil
-VPDmin_warm <- (
-  prism$PRISM_vpdmin_30yr_normal_800mM2_05_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_06_bil + 
-  prism$PRISM_vpdmin_30yr_normal_800mM2_07_bil) / 3
-VPDmax_warm <- (
-  prism$PRISM_vpdmax_30yr_normal_800mM2_05_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_06_bil + 
-  prism$PRISM_vpdmax_30yr_normal_800mM2_07_bil) / 3
-VPD_warm <- (VPDmax_warm + VPDmin_warm) / 2
+# Create water year normals
+PPT_year <- # cumulative precipitation
+  prism$PRISM_ppt_30yr_normal_4kmM2_09_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_10_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_11_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_12_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_01_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_02_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_03_bil +
+  prism$PRISM_ppt_30yr_normal_4kmM2_04_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_05_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_06_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_07_bil + 
+  prism$PRISM_ppt_30yr_normal_4kmM2_08_bil
+T_year <- ( # average T
+  prism$PRISM_tmean_30yr_normal_4kmM2_09_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_10_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_11_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_12_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_01_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_02_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_03_bil +
+  prism$PRISM_tmean_30yr_normal_4kmM2_04_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_05_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_06_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_07_bil + 
+  prism$PRISM_tmean_30yr_normal_4kmM2_08_bil) / 12
 
 # Crop normals to extent of BA (for better visualization)
-BA <- raster("D:/EvansLab/Final/Data/BA/BA.tif")
+BA <- raster("C:/Users/mekevans/Documents/old_user/Documents/CDrive/Bayes/DemogRangeMod/ProofOfConcept/FIA-data/westernData/NewData/IWStates/PiedIPM/MEKEvans/BA/BA.tif")
 ext <- extent(BA)
-PPT_cool <- crop(PPT_cool, ext)
-PPT_warm <- crop(PPT_warm, ext)
-VPD_cool <- crop(VPD_cool, ext)
-VPD_warm <- crop(VPD_warm, ext)
+PPT_year <- crop(PPT_year, ext)
+T_year <- crop(T_year, ext)
 
 # Export seasonal normals
-writeRaster(PPT_cool, "D:/EvansLab/Final/Data/PRISM/Normals/PPT_cool.tif", overwrite = T)
-writeRaster(PPT_warm, "D:/EvansLab/Final/Data/PRISM/Normals/PPT_warm.tif", overwrite = T)
-writeRaster(VPD_cool, "D:/EvansLab/Final/Data/PRISM/Normals/VPD_cool.tif", overwrite = T)
-writeRaster(VPD_warm, "D:/EvansLab/Final/Data/PRISM/Normals/VPD_warm.tif", overwrite = T)
+writeRaster(PPT_year, paste0(PRISM.norm.path, "PPT_year.tif"), overwrite = T)
+writeRaster(T_year, paste0(PRISM.norm.path, "T_year.tif"), overwrite = T)
--- Code/ClimateProcessing/historic.R
+++ Code/ClimateProcessing/historic.R
@@ -1,11 +1,15 @@
 library(raster)
 
+### PRISM download June 14, 2018
+### January 1981 through November 2017
+### (36*12) + 11 = 443 files
+
 # Search for PRISM files
-climateDir <- "D:/EvansLab/Final/Data/PRISM/Historic/"
-pptFiles <- list.files(path = climateDir, pattern = glob2rx("*ppt*.bil"), full.names = TRUE)
-tmpFiles <- list.files(path = climateDir, pattern = glob2rx("*tmean*.bil"), full.names = TRUE)
-vpdminFiles <- list.files(path = climateDir, pattern = glob2rx("*vpdmin*.bil"), full.names = TRUE)
-vpdmaxFiles <- list.files(path = climateDir, pattern = glob2rx("*vpdmax*.bil"), full.names = TRUE)
+PRISM.path <-  "E:/Bayes/DemogRangeMod/ProofOfConcept/FIA-data/westernData/NewData/IWStates/PiedIPM/MEKEvans/ClimateData/PRISM/"
+pptFiles <- list.files(path = PRISM.path, pattern = glob2rx("*ppt*.bil"), full.names = TRUE)
+tmpFiles <- list.files(path = PRISM.path, pattern = glob2rx("*tmean*.bil"), full.names = TRUE)
+#vpdminFiles <- list.files(path = PRISM.path, pattern = glob2rx("*vpdmin*.bil"), full.names = TRUE)
+vpdmaxFiles <- list.files(path = PRISM.path, pattern = glob2rx("*vpdmax*.bil"), full.names = TRUE)
 
 # Stack monthly data
 pptStack <- stack()
@@ -13,24 +17,27 @@ for (i in pptFiles) {
   print(i)
   pptStack <- stack(pptStack, raster(i))
 }
+
 tmpStack <- stack()
 for (i in tmpFiles) {
   print(i)
   tmpStack <- stack(tmpStack, raster(i))
 }
+
 vpdStack <- stack()
-for (i in 1:length(vpdminFiles)) {
+for (i in 1:length(vpdmaxFiles)) {
   print(i)
-  vpdStack <- stack(vpdStack, (raster(vpdminFiles[i]) + raster(vpdmaxFiles[i])) / 2)
+  vpdStack <- stack(vpdStack, raster(vpdmaxFiles[i]))
 }
 
-# Crop to extent of FIA Pinus edulis occurences
-cropExtent <- extent(raster("D:/EvansLab/Final/Data/BA/BA.tif"))
+# Crop to extent of FIA Pinus edulis occurrences
+cropExtent <- extent(raster("C:/Users/mekevans/Documents/old_user/Documents/CDrive/Bayes/DemogRangeMod/ProofOfConcept/FIA-data/westernData/NewData/IWStates/PiedIPM/MEKEvans/BA/BA.tif"))
 pptStackCropped <- crop(pptStack, cropExtent)
 tmpStackCropped <- crop(tmpStack, cropExtent)
 vpdStackCropped <- crop(vpdStack, cropExtent)
 
 # Export rasters
-writeRaster(pptStackCropped, paste0(climateDir, "pptStack2.tif"), overwrite = T)
-writeRaster(tmpStackCropped, paste0(climateDir, "tmpStack2.tif"), overwrite = T)
-writeRaster(vpdStackCropped, paste0(climateDir, "vpdStack2.tif"), overwrite = T)
+clim.path <-  "E:/Bayes/DemogRangeMod/ProofOfConcept/FIA-data/westernData/NewData/IWStates/PiedIPM/MEKEvans/ClimateData/"
+writeRaster(pptStackCropped, paste0(clim.path, "pptStack.tif"), overwrite = T)
+writeRaster(tmpStackCropped, paste0(clim.path, "tmpStack.tif"), overwrite = T)
+writeRaster(vpdStackCropped, paste0(clim.path, "vpdStack.tif"), overwrite = T)
